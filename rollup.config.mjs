64af659811fca6676586a609d59d4a4411b133ae
Vitalii <vitalya199529@gmail.com>
2023-09-12 23:54:56 +0900

    Housekeeping.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,4 +1,4 @@
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import peerDepsExternal from "@chrisneedham/rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
