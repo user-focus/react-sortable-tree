a582ef764e70b16177adacd3f97b059e3b7cd56f
Vitalii <vitalya199529@gmail.com>
2024-03-04 08:59:42 +0900

    esm only

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,10 +1,8 @@
-import peerDepsExternal from "@chrisneedham/rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
 import dts from "rollup-plugin-dts";
-import terser from "@rollup/plugin-terser";
 
 // This is required to read package.json file when
 // using Native ES modules in Node.js
@@ -18,25 +16,19 @@ export default [{
   input: "src/index.ts",
   output: [
     {
-      file: packageJson.main,
-      format: "cjs",
-      sourcemap: true
-    },
-    {
       file: packageJson.module,
       format: "esm",
       sourcemap: true
     }
   ],
+  external: [/node_modules/],
   plugins: [
-    peerDepsExternal(),
     resolve(),
     commonjs(),
     typescript(),
     postcss({
       extensions: ['.css']
     }),
-    terser(),
   ]
 }, {
   input: 'lib/index.d.ts',
